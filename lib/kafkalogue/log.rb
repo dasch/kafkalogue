bb1ec2c214f4e482c5ad5f14070cc1849b6bff7c
Daniel Schierbeck <dasch@zendesk.com>
2015-09-30 13:35:45 +0100

    Make the buffer bounded & instrument overflows

--- lib/kafkalogue/log.rb
+++ lib/kafkalogue/log.rb
@@ -1,9 +1,10 @@
 require 'poseidon'
 
 module Kafkalogue
-  class Log
-    PRODUCER_NAME = "kafkalogue"
+  PRODUCER_NAME = "kafkalogue"
+  MAX_BUFFER_SIZE = 5000
 
+  class Log
     def initialize(brokers:, topic:)
       @producer = Poseidon::Producer.new(brokers, PRODUCER_NAME, {
         compression_codec: :snappy
@@ -14,12 +15,26 @@ module Kafkalogue
     end
 
     def write(data, key:)
-      @buffer << Poseidon::MessageToSend.new(@topic, data, key.to_s)
+      if @buffer.size < MAX_BUFFER_SIZE
+        @buffer << Poseidon::MessageToSend.new(@topic, data, key.to_s)
+      else
+        instrument :buffer_overflow
+      end
     end
 
     def flush
       @producer.send_messages(@buffer)
       @buffer.clear
+    end
+
+    private
+
+    def instrument(event, payload = {})
+      if defined?(ActiveSupport::Notifications)
+        key = "#{event}.kafkalogue"
+        payload[:topic] = @topic
+        ActiveSupport::Notifications.instrument(key, payload)
+      end
     end
   end
 end
--- spec/kafkalogue_spec.rb
+++ spec/kafkalogue_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
+require 'active_support/notifications'
 
 describe Kafkalogue do
+  let(:kafka_brokers) { ENV.fetch("KAFKA_BROKERS").split(",") }
+  let(:log) { Kafkalogue.new(brokers: kafka_brokers, topic: "test") }
+
   it "writes log entries" do
-    kafka_brokers = ENV.fetch("KAFKA_BROKERS").split(",")
-    log = Kafkalogue.new(brokers: kafka_brokers, topic: "test")
     log.write("some data", key: "yolo")
     log.flush
+  end
+
+  it "instruments buffer overflows" do
+    events = []
+
+    subscriber = -> (*args) do
+      events << ActiveSupport::Notifications::Event.new(*args)
+    end
+
+    ActiveSupport::Notifications.subscribe("buffer_overflow.kafkalogue", subscriber)
+
+    Kafkalogue::MAX_BUFFER_SIZE.times do
+      log.write("yolo", key: "xoxo")
+    end
+
+    expect(events).to be_empty
+
+    log.write("hey", key: "you")
+
+    expect(events.size).to eq 1
+
+    event = events.first
+
+    expect(event.payload.fetch(:topic)).to eq "test"
   end
 end
